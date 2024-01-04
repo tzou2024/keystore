4093cf99a8be5919387eb2f02f3344fbc09837a4
tzou2024 <tzou@olin.edu>
2024-01-03 23:45:02 -0500

    Add transaction logger, TODO integrate into service

--- logger.go
+++ logger.go
@@ -1,6 +1,152 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 type TransactionLogger interface {
 	WriteDelete(key string)
 	WritePut(key, value string)
+	Err() <-chan error
+
+	ReadEvents() (<-chan Event, <-chan error)
+
+	Run()
+}
+
+type EventType byte
+
+const (
+	_                     = iota
+	EventDelete EventType = iota
+	EventPut
+)
+
+type Event struct {
+	Sequence  uint64
+	EventType EventType
+	Key       string
+	Value     string
+}
+
+type FileTranscationLogger struct {
+	events       chan<- Event //Write-only channel for sending events
+	errors       <-chan error //Read-only channel for receiving errors
+	lastSequence uint64       // the last used event sequence number
+	file         *os.File     // Location of the log
+}
+
+func (l *FileTranscationLogger) WritePut(key, value string) {
+	l.events <- Event{EventType: EventPut, Key: key, Value: value}
+}
+
+func (l *FileTranscationLogger) WriteDelete(key string) {
+	l.events <- Event{EventType: EventDelete, Key: key}
+}
+
+func (l *FileTranscationLogger) Err() <-chan error {
+	return l.errors
+}
+
+func NewFileTransactionLogger(filename string) (TransactionLogger, error) {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
+	if err != nil {
+		return nil, fmt.Errorf("can't open transaction log file: %w", err)
+	}
+	return &FileTranscationLogger{file: file}, nil
+}
+
+func (l *FileTranscationLogger) Run() {
+	//make events and errors channels
+	events := make(chan Event, 16)
+	l.events = events
+
+	errors := make(chan error, 1)
+	l.errors = errors
+
+	go func() {
+		for e := range events {
+			l.lastSequence++
+
+			_, err := fmt.Fprintf(
+				l.file,
+				"%d\t%d\t%s\t%s\n",
+				l.lastSequence, e.EventType, e.Key, e.Value)
+
+			if err != nil {
+				errors <- err
+				return
+			}
+
+		}
+	}()
+}
+
+func (l *FileTranscationLogger) ReadEvents() (<-chan Event, <-chan error) {
+	scanner := bufio.NewScanner(l.file)
+
+	// create an unbuffered event and buffered error channel
+	outEvent := make(chan Event)
+	outError := make(chan error, 1)
+
+	go func() {
+		var e Event
+		defer close(outEvent)
+		defer close(outError)
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if _, err := fmt.Sscanf(line, "%d\t%d\t%s\t%s",
+				&e.Sequence, &e.EventType, &e.Key, &e.Value); err != nil {
+				outError <- fmt.Errorf("input parse error: %w", err)
+				return
+			}
+
+			// Sanity check! Are the sequence numbers in increasing order?
+			if l.lastSequence >= e.Sequence {
+				outError <- fmt.Errorf("transaction numbers out of sequence")
+				return
+			}
+
+			l.lastSequence = e.Sequence
+
+			outEvent <- e
+		}
+		if err := scanner.Err(); err != nil {
+			outError <- fmt.Errorf("transaction log read failure: %w", err)
+			return
+		}
+	}()
+	return outEvent, outError
+}
+
+var logger TransactionLogger
+
+func initalizeTransactionLog() error {
+	var err error
+
+	logger, err = NewFileTransactionLogger("transaction.log")
+	if err != nil {
+		return fmt.Errorf("failed to create event logger: %w", err)
+	}
+
+	events, errors := logger.ReadEvents()
+	e, ok := Event{}, true
+
+	if ok && err == nil {
+		select {
+		case err, ok = <-errors: // Retreive any errors
+		case e, ok = <-events:
+			switch e.EventType {
+			case EventDelete:
+				err = Delete(e.Key) //Got a Delete event!
+			case EventPut:
+				err = Put(e.Key, e.Value) // Got a PUT
+			}
+		}
+	}
+	logger.Run()
+	return err
 }
